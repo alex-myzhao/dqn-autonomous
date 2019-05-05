ab25ee27c0539c914f162d6533ba506fc7152450
alex-myzhao <mingyusysu@gmail.com>
2019-05-05 00:20:43 -0400

    testing dqn algorithm

--- agent.py
+++ agent.py
@@ -5,19 +5,21 @@ import random
 import cv2
 
 class Agent:
+    ACTION_SPACE = [(0, 0.1), (-25, 0.1), (25, 0.1), (0, 0.2)]
+
     def __init__(self, options=None):
         # -- Basic settings related to the environment --
-        self.num_actions = 3
-        self.action_space = (0, 1, 2)
+        self.num_actions = len(Agent.ACTION_SPACE)
         # -- TD trainer options --
-        self.learning_rate = 0.01                 # learning rate
-        self.batch_size = 8              # batch size
+        self.learning_rate = 0.01         # learning rate
+        self.momentum = 0.8               # momentum
+        self.batch_size = 8               # batch size
         self.l2_decay = 0.01              # L2 normalization
         # -- other options --
         self.temporal_window = 3          # number of pervious states an agent remembers
         self.gamma = 0.7                  # future discount for reward
         self.epsilon = 0.3                # epsilon during training
-        self.start_learn_threshold = 200  # minimum number of examples in replay memory before learning
+        self.start_learn_threshold = 10  # minimum number of examples in replay memory before learning
         self.experience_size = 3000       # size of replay memory
         self.learning_steps_burnin = 1000 # number of random actions the agent takes before learning
         self.learning_steps_total = 10000 # number of training iterations
@@ -29,66 +31,65 @@ class Agent:
         self.step = 0
 
     def _build_model(self):
+        opt = keras.optimizers.SGD(lr=self.learning_rate, momentum=self.momentum)
         model = keras.Sequential()
         model.add(keras.layers.Conv2D(32, (3, 3), padding='same'))
         model.add(keras.layers.Activation('relu'))
+        model.add(keras.layers.MaxPooling2D(pool_size=(2, 2), padding='valid'))
         model.add(keras.layers.Conv2D(16, (3, 3), padding='same'))
         model.add(keras.layers.Activation('relu'))
         model.add(keras.layers.Flatten())
         model.add(keras.layers.Dense(self.num_actions))
         model.add(keras.layers.Activation('softmax'))
-        model.compile(loss='mse', optimizer=keras.optimizers.Adam(lr=self.learning_rate))
+        model.compile(loss='mse', optimizer=opt)
         return model
 
-    def feedforward(self, state):
-        state = np.expand_dims(state, axis=0)
+    def _forward(self, state):
         prediction = self._model.predict(state)
-        return prediction[0]
+        return prediction
 
-    def backward(self, state, target):
-        state = np.expand_dims(state, axis=0)
-        target = np.expand_dims(target, axis=0)
-        self._model.fit(state, target, epochs=1, verbose=0)
+    def _backward(self, state, target):
+        history = self._model.fit(state, target, epochs=1, verbose=0)
+        print(history.history)
 
-    def remember(self, state, action, reward, next_state):
+    def _remember(self, state, action, reward, next_state):
         index = self.step % self.experience_size
-        cv2.imwrite('./_debug/' + str(self.step) + '_' + str(action) + '_' + str(reward) + '.png', state)
-        # cv2.imwrite('./_debug/' + str(self.step) + 'next.png', next_state)
+        # cv2.imwrite('./_debug/' + str(self.step) + '_' + str(action) + '_' + str(reward) + '.png', state)
         self._memory[index] = (state, action, reward, next_state)
 
     def learn(self, state, action, reward, next_state):
-        self.remember(state, action, reward, next_state)
-        chosen_action = 1
-        if self.step < self.start_learn_threshold:
-            chosen_action = random.sample(self.action_space, 1)[0]
-        else:
-            # sample from the replay memory
+        self._remember(state, action, reward, next_state)
+        if self.step >= self.start_learn_threshold:
             minibatch = random.sample(self._memory[0:self.step], self.batch_size)
-            for state, action, reward, next_state in minibatch:
-                q_hat = self.feedforward(next_state)
-                td_target = reward + self.gamma * np.amax(q_hat)
-                prediction = self.feedforward(state)
-                prediction[action] = td_target
-                self.backward(state, prediction)
-            chosen_action = self.act(next_state)
+            states = np.array([dp[0] for dp in minibatch])
+            actions = np.array([dp[1] for dp in minibatch])
+            rewards = np.array([dp[2] for dp in minibatch])
+            next_states = np.array([dp[3] for dp in minibatch])
+            q_hats = self._forward(next_states)
+            td_targets = rewards + self.gamma * np.amax(q_hats)
+            predictions = self._forward(states)
+            for i in range(len(predictions)):
+                predictions[i][actions[i]] = td_targets[i]
+            self._backward(states, predictions)
         self.step += 1
-        return chosen_action
 
     def act(self, state):
-        if np.random.rand() <= self.epsilon:
-            # Random exploration
-            return random.sample(self.action_space, 1)[0]
+        if self.step < self.learning_steps_burnin or np.random.rand() <= self.epsilon:
+            return random.randint(0, self.num_actions - 1)
         else:
-            # Predict the reward value based on the given state
-            act_values = self.feedforward(state)
-            # Pick the action based on the predicted reward
+            act_values = self._forward(np.array([state]))
             return np.argmax(act_values)
 
-    def is_training(self):
-        return self.step >= self.start_learn_threshold
+    def get_reward(self, cte):
+        if abs(cte) >= 2.5:
+            return -100
+        elif abs(cte) >= 1.0:
+            return 0
+        else:
+            return 10
 
-    def save(self):
-        pass
+    def load(self, name):
+        self._model.load_weights(name)
 
-    def load(self):
-        pass
+    def save(self, name):
+        self._model.save_weights(name)
--- server.py
+++ server.py
@@ -13,71 +13,46 @@ sio = socketio.Server()
 app = Flask(__name__)
 
 dqn_agent = Agent()
+episode = 0
 counter = 0
-steer_buffer = 0
-data_buffer = None
+timestep = 0
+cur_control = [0, 0]
+buffer = [None, None] # last state, last action
 INTERVAL = 25
-
-
-def get_action(steer):
-    if steer < -15:
-        return 0
-    elif steer > 15:
-        return 2
-    else:
-        return 1
-
-
-def get_steer(action):
-    steer_list = [-25.0, 0.0, 25.0]
-    return steer_list[action]
-
-
-def get_reward(cte, speed):
-    return -(cte ** 2) * 10 + speed
-
-
-def dqn(dqn_agent, data, next_data):
-    state = cv2.resize(cv2.imdecode(np.frombuffer(base64.b64decode(data['image']), np.uint8), cv2.IMREAD_COLOR), (160, 80))
-    steer = float(data['steering_angle'])
-    reward = float(next_data['cte'])
-    # reward = get_reward(float(next_data['cte']), float(next_data['speed']))
-    next_state = cv2.resize(cv2.imdecode(np.frombuffer(base64.b64decode(next_data['image']), np.uint8), cv2.IMREAD_COLOR), (160, 80))
-    # print(get_action(steer), reward)
-    return get_steer(dqn_agent.learn(state, get_action(steer), reward, next_state))
+INIT_THRESHOLD = 3
+FINISH_THRESHOLD = 2.5
 
 
 @sio.on('telemetry')
 def telemetry(sid, data):
     global counter
-    global steer_buffer
-    global data_buffer
-    global dqn_agent
+    global timestep
+    global episode
+    global buffer
+    global cur_control
     if data:
-        if data_buffer == None:
-            data_buffer = data
-            return
-        # safety control
-        cte = float(data['cte'])
-        if counter % INTERVAL == 0:
-            v_pre, v = float(data_buffer['speed']), float(data['speed'])
-            if v - v_pre < 0.1 and v < 0.1:
-                reset()
-                return
-            if -2.2 < cte < 2.2:
-                # DQN
-                steer_buffer = dqn(dqn_agent, data_buffer, data)
+        timestep += 1
+        if timestep % INTERVAL == 0:
+            counter += 1
+            print('episode {}, step {}'.format(episode, counter))
+            if counter <= INIT_THRESHOLD:
+                cur_control = [0.0, 0.2]
+                new_state = cv2.resize(cv2.imdecode(np.frombuffer(base64.b64decode(data['image']), np.uint8), cv2.IMREAD_COLOR), (160, 80))
+                buffer[0], buffer[1] = new_state, 0
             else:
-                if cte >= 2.2:
-                    steer_buffer = -25.0
-                    data['steering_angle'] = -25.0
-                elif cte <= -2.2:
-                    steer_buffer = 25.0
-                    data['steering_angle'] = 25.0
-                data['ctx'] = 10
-            data_buffer = data
-        send_control(steer_buffer, 0.1)
-        counter += 1
+                cte = float(data['cte'])
+                if -FINISH_THRESHOLD < cte < FINISH_THRESHOLD:
+                    state, action = buffer[0], buffer[1]
+                    new_state = cv2.resize(cv2.imdecode(np.frombuffer(base64.b64decode(data['image']), np.uint8), cv2.IMREAD_COLOR), (160, 80))
+                    reward = dqn_agent.get_reward(cte)
+                    new_action = dqn_agent.act(new_state)
+                    # buffer the current result
+                    buffer[0], buffer[1] = new_state, new_action
+                    dqn_agent.learn(state, action, reward, new_state)
+                    cur_control = Agent.ACTION_SPACE[new_action]
+                else:
+                    reset()
+        send_control(cur_control[0], cur_control[1])
     else:
         sio.emit('manual', data={}, skip_sid=True)
 
@@ -93,17 +68,24 @@ def send_control(steering_angle, throttle):
 
 
 def reset():
-    global data_buffer
-    print('reset')
-    # reset all global settings
-    data_buffer = None
-    # storage buffered data
+    global episode
+    global timestep
+    global buffer
+    global dqn_agent
+    global cur_control
+    global counter
+    episode += 1
+    print('reset, start episode: {}'.format(episode))
+    timestep, counter = 0, 0
+    buffer = [None, None]
+    cur_control = [0.0, 0.0]
+    if episode % 10 == 0:
+        dqn_agent.save('./model/autonomous.h5')
     sio.emit('reset', {})
 
 
 if __name__ == '__main__':
     # wrap Flask application with engineio's middleware
     app = socketio.Middleware(sio, app)
-
     # deploy as an eventlet WSGI server
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
